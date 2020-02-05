b0119c3b547c137e9200d3929e010cc6ab9e3d8c
Dmitriy <d.orlov777@gmail.com>
2020-02-05 14:00:32 +0300

    Правит id

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/app/app.jsx';
 
-const rootElement = document.getElementById(`#root`);
+const rootElement = document.getElementById(`root`);
 const PLACES_COUNT = 312;
 
 ReactDom.render(<App placesCount={PLACES_COUNT} />, rootElement);
