dd54b859855b14741f2d598c5ce7d4f8e4f8887e
Dmitriy <d.orlov777@gmail.com>
2020-02-08 16:00:06 +0300

    Создает снепшот тесты

---
+++ src/components/app/app.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from './app.jsx';
+
+const PLACES_COUNT = 100;
+const PLACE_CARD_NAMES = [`placeCardName1`, `placeCardName2`];
+
+it(`<App /> should be render correctly`, () => {
+	const app = renderer.create(<App placesCount={PLACES_COUNT} placeCardNames={PLACE_CARD_NAMES}/>).toJSON();
+	expect(app).toMatchSnapshot();
+});
---
+++ src/components/main/main.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Main from './main.jsx';
+
+const PLACES_COUNT = 100;
+const PLACE_CARD_NAMES = [`placeCardName4`, `placeCardName5`];
+
+it(`<Main /> should be render correctly`, () => {
+	const main = renderer.create(<Main placesCount={PLACES_COUNT} placeCardNames={PLACE_CARD_NAMES}/>).toJSON();
+	expect(main).toMatchSnapshot()
+});
