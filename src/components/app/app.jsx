39a718f73591f63a45079399d9fedb3ab41d5266
Dmitriy <d.orlov777@gmail.com>
2020-02-15 14:41:21 +0300

    Добавляет и подключает react-router-dom. Добавляет компонент CardProperty

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
-import Main from '../main/main.jsx';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Main from '../main/main.jsx';
+import CardProperty from '../card-property/card-property.jsx';
 
 const handleCardClick = () => {};
 
-const App = ({placesCount, offerList}) => {
-  return <Main placesCount={placesCount} offerList={offerList} handleCardClick={handleCardClick} />;
-};
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+			  <Switch>
+			    <Route exact path="/">
+			      {this._renderApp()}
+			    </Route>
+			    <Route exact path="/dev-card-property">
+			      <CardProperty />
+			    </Route>
+			  </Switch>
+			</BrowserRouter>)
+  }
+
+  _renderApp() {
+  	const { placesCount, offerList } = this.props;
+
+    return <Main placesCount={placesCount} offerList={offerList} handleCardClick={handleCardClick} />;
+  }
+}
 
 App.propTypes = {
   placesCount: PropTypes.number.isRequired,
