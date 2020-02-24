8331e6c8cd25ff252080de28f3f7f266ff5f905d
Dmitriy <d.orlov777@gmail.com>
2020-02-24 09:20:43 +0300

    Мокает leaflet

---
+++ src/__mocks__/leaflet.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const leaflet = jest.genMockFromModule('leaflet');
+
+leaflet.fn = {
+    addLayer: jest.fn(),
+    removeLayer: jest.fn()
+};
+
+leaflet.icon = () => {};
+
+leaflet.map = () => {
+    return {
+        setView: () => {},
+        remove: () => {},
+        removeLayer: leaflet.fn.removeLayer,
+    };
+};
+
+leaflet.marker = () => {
+    return {
+        addTo: leaflet.fn.addLayer,
+    };
+};
+
+leaflet.tileLayer = () => {
+    return {
+        addTo: () => {},
+    };
+};
+
+module.exports = leaflet;
