045a5520caec731473390c470b3f78e65a80976b
Dmitriy <d.orlov777@gmail.com>
2020-02-27 20:25:40 +0300

    Добавляет в тесты стор новые экшены

--- src/action-creator.test.js
+++ src/action-creator.test.js
@@ -1,24 +1,37 @@
 import ActionCreator from './action-creator.js';
 import ActionType from './action-type.js';
-import {ScreenType} from './const.js';
+import {ScreenType, SortType} from './const.js';
 
 describe(`Test ActionCreator`, () => {
   it(`ActionCreator.changeCity should return a right action`, () => {
     expect(ActionCreator.changeCity(1)).toEqual({type: ActionType.CHANGE_CITY, payload: 1});
   });
 
-  it(`ActionCreator.selectOffe should return a right actionrs`, () => {
+  it(`ActionCreator.selectOffers should return a right action`, () => {
     expect(ActionCreator.selectOffers()).toEqual({type: ActionType.SELECT_OFFERS});
   });
 
-  it(`ActionCreator.changeActi should return a right actionveCard`, () => {
+  it(`ActionCreator.changeActiveCard should return a right action`, () => {
     expect(ActionCreator.changeActiveCard(`offer`)).toEqual({type: ActionType.CHANGE_ACTIVE_CARD, payload: `offer`});
   });
 
-  it(`ActionCreator.changeScre should return a right actionenType`, () => {
+  it(`ActionCreator.changeScreenType should return a right action`, () => {
     expect(ActionCreator.changeScreenType(ScreenType.MAIN)).toEqual({
       type: ActionType.CHANGE_SCREEN_TYPE,
       payload: ScreenType.MAIN
+    });
+  });
+
+  it(`ActionCreator.changeSortType should return a right action`, () => {
+    expect(ActionCreator.changeSortType(SortType.POPULAR)).toEqual({
+      type: ActionType.CHANGE_SORT_TYPE,
+      payload: SortType.POPULAR
+    });
+  });
+
+  it(`ActionCreator.sortOffers should return a right action`, () => {
+    expect(ActionCreator.sortOffers()).toEqual({
+      type: ActionType.SORT_OFFERS
     });
   });
 });
