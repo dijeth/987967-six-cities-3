b57951e5cf8fa14c05c7704465c704e719a2bfe1
Dmitriy <d.orlov777@gmail.com>
2020-02-27 15:06:12 +0300

    wip

---
+++ src/components/sort-list/sort-list.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {SortList} from './sort-list.jsx';
+import {SortType} from '../../const.js';
+
+const tree = renderer.create(<SortList />)
+
+it(`<SortList /> should be correctly rendered when closed`, () => {
+	expect(tree.toJSON()).toMatchShanshot()
+});
+
