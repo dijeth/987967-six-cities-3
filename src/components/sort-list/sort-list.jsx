dcd8026648b2856b37e937244f9faa6227a5a631
Dmitriy <d.orlov777@gmail.com>
2020-02-27 18:22:59 +0300

    Рефакторит sort-list

--- src/components/sort-list/sort-list.jsx
+++ src/components/sort-list/sort-list.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { SortType } from '../../const.js';
-import { indexOf } from '../../util.js';
 import { connect } from 'react-redux';
 import ActionCreator from '../../action-creator.js';
 
@@ -21,23 +20,10 @@ class SortList extends React.PureComponent {
     };
 
     this._handleSortTypeClick = this._handleSortTypeClick.bind(this);
-    this._handleSortListClick = this._handleSortListClick.bind(this);
   }
 
   _handleSortTypeClick() {
     this.setState((state) => ({ isOpen: !state.isOpen }))
-  }
-
-  _handleSortListClick(evt) {
-    const index = indexOf(evt.target);
-
-    this.setState({
-      isOpen: false
-    });
-
-    if (index !== undefined) {
-      this.props.onSortTypeChange(SORT_LIST[index]);
-    };
   }
 
   render() {
@@ -45,7 +31,12 @@ class SortList extends React.PureComponent {
 
     const sortList = SORT_LIST.map((it, i) => {
       const className = `places__option ${it === activeType ? `places__option--active` : ``}`;
-      return <li className={className} tabIndex="0" key={`${it}-${i}`}>{it}</li>
+      const handler = () => {
+        onSortTypeChange(it);
+        this.setState({ isOpen: false })
+      };
+
+      return <li className={className} tabIndex="0" onClick={handler} key={`${it}-${i}`}>{it}</li>
     });
 
     return (
@@ -90,5 +81,5 @@ const mapDispatchToProps = (dispatch) => ({
   }
 })
 
-export {SortList};
+export { SortList };
 export default connect(mapStateToProps, mapDispatchToProps)(SortList);
