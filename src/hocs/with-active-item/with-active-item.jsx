91b54719a50235a2477b0afd979c7a8701d0227c
Dmitriy <d.orlov777@gmail.com>
2020-02-28 16:16:50 +0300

    Добавляет в with-active-item возможность запуска нескольких обработчиков

--- src/hocs/with-active-item/with-active-item.jsx
+++ src/hocs/with-active-item/with-active-item.jsx
@@ -2,65 +2,88 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const isChild = (element, parentElement) => {
-	 return element.parentElement === parentElement;
+  return element.parentElement === parentElement;
 }
 
 const getChild = (targetElement, parentElement) => {
-	while (targetElement !== document.body && targetElement !== parentElement && !isChild(targetElement, parentElement)) {
-		targetElement = targetElement.parentElement
-	};
+  while (targetElement !== document.body && targetElement !== parentElement && !isChild(targetElement, parentElement)) {
+    targetElement = targetElement.parentElement
+  };
 
-	if (targetElement === parentElement || targetElement === document.body) {
-		return null
-	};
+  if (targetElement === parentElement || targetElement === document.body) {
+    return null
+  };
 
-	return targetElement;
+  return targetElement;
 }
 
 const getChildIndex = (targetElement, parentElement) => {
-	const child = getChild(targetElement, parentElement);
+  const child = getChild(targetElement, parentElement);
 
-	if (child === null) {
-		return null
-	};
+  if (child === null) {
+    return null
+  };
 
-	return Array.from(parentElement.children).indexOf(child);
+  return Array.from(parentElement.children).indexOf(child);
+}
+
+const normalizeHandlerProp = (handlerProp) => {
+  if (handlerProp === undefined || handlerProp === null) {
+    return null
+  };
+
+  if (handlerProp.constructor.name) {
+    return [handlerProp];
+  };
+
+  return handlerProp;
 }
 
 const withActiveItem = (ListComponent) => {
-	class WithActiveItem extends React.PureComponent {
-		constructor(props) {
-			super(props);
+  class WithActiveItem extends React.PureComponent {
+    constructor(props) {
+      super(props);
 
-			this.state = {
-				activeIndex: props.activeItem
-			};
+      this.state = {
+        activeIndex: props.activeItem
+      };
 
-			this._handleClick = this._handleClick.bind(this);
-		}
+      this._handleClick = this._handleClick.bind(this);
+    }
 
-		_handleClick(evt) {
-			const parentElement = evt.currentTarget;
-			const element = evt.target;
-			const activeIndex = getChildIndex(element, parentElement);
+    _handleClick(evt) {
+      const parentElement = evt.currentTarget;
+      const element = evt.target;
+      const activeIndex = getChildIndex(element, parentElement);
 
-			this.setState({activeIndex});
+      this.setState({ activeIndex });
 
-			if (activeIndex !== null && this.props.onActiveItemChange) {
-				this.props.onActiveItemChange(activeIndex)
-			};
-		}
+      const handlers = normalizeHandlerProp(this.props.onActiveItemChange);
 
-		render() {
-			const {onListClick, activeItem, ...props} = this.props;
+      if (activeIndex !== null && handlers !== null) {
+      	handlers.forEach((it) => {
+      		it(activeIndex)
+      	})
+      };
+    }
 
-			return <ListComponent activeItem={this.state.activeIndex} onListClick={this._handleClick} {...props} />
-		}
-	};
+    render() {
+      const { onListClick, activeItem, ...props } = this.props;
 
-	return WithActiveItem;
+      return <ListComponent activeItem={this.state.activeIndex} onListClick={this._handleClick} {...props} />
+    }
+  };
 
-	WithActiveItem.propTypes = {};
+  return WithActiveItem;
+
+  WithActiveItem.propTypes = {
+    activeItem: PropTypes.number,
+    onListClick: PropTypes.func.isRequired,
+    onActiveItemChange: PropTypes.oneOfType([
+      PropTypes.func,
+      PropTypes.arrayOf(PropTypes.func)
+    ])
+  };
 };
 
 export default withActiveItem;
