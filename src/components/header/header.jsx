46eec53213df694feee551bbfcc99a07f68e25d7
Dmitriy <d.orlov777@gmail.com>
2020-03-12 09:51:04 +0300

    refactoring header.jsx

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -12,20 +12,22 @@ const Header = ({isAuthorized, userPicture, email, isUserSuper, isActiveLogo}) =
 
   const link = isAuthorized ? AppRoute.getFavorites() : AppRoute.getLogin();
 
+  const inactiveLogoBLock = (
+    <a className="header__logo-link header__logo-link--active">
+      <img className="header__logo" src="/img/logo.svg" alt="6 cities logo" width="81" height="41"/>
+    </a>);
+
+  const activeLogoBlock = (
+    <Link to={AppRoute.getRoot()} className="header__logo-link">
+      <img className="header__logo" src="/img/logo.svg" alt="6 cities logo" width="81" height="41"/>
+    </Link>);
+
   return (
     <header className="header">
       <div className="container">
         <div className="header__wrapper">
           <div className="header__left">
-            {!isActiveLogo && (
-              <Link to={AppRoute.getRoot()} className="header__logo-link">
-                <img className="header__logo" src="/img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-              </Link>)}
-
-            {isActiveLogo && (
-              <a className="header__logo-link header__logo-link--active">
-                <img className="header__logo" src="/img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-              </a>)}
+            {isActiveLogo ? inactiveLogoBLock : activeLogoBlock}
           </div>
           <nav className="header__nav">
             <ul className="header__nav-list">
