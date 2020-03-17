907c52ab069556befd5a62454e9206dd70403a0d
Dmitriy <d.orlov777@gmail.com>
2020-03-17 16:50:20 +0300

    adds private route

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {Router, Route, Switch} from 'react-router-dom';
 import PageMain from '../page-main/page-main.jsx';
 import PageFavotites from '../page-favorites/page-favorites.jsx';
 import PageProperties from '../page-properties/page-properties.jsx';
 import PageSignIn from '../page-sign-in/page-sign-in.jsx';
 import history from '../../history.js';
-
 import withPathName from '../../hocs/with-pathname/with-pathname.jsx';
+import {getAuthorizationStatus} from '../../reducers/user/selectors.js';
+import {connect} from 'react-redux';
+import PrivateRoute from '../private-route/private-route.jsx';
+import {AppRoute} from '../../const/const.js';
 
 const PagePropertiesWithPathName = withPathName(PageProperties);
 
-const App = () => {
+const App = ({isAuth}) => {
   return (
     <Router history={history}>
       <Switch>
@@ -18,19 +22,19 @@ const App = () => {
           const id = match.params.id;
           return <PagePropertiesWithPathName pathID={id} />;
         }} />
-        <Route exact path="/login">
-          <PageSignIn />
-        </Route>
-        <Route exact path="/favorites">
-          <PageFavotites />
-        </Route>
-        <Route exact path="/">
-          <PageMain />
-        </Route>
+        <PrivateRoute path="/login" require={!isAuth} render={() => <PageSignIn />} to={AppRoute.getRoot()} />
+        <PrivateRoute path="/favorites" require={isAuth} render={() => <PageFavotites />} to={AppRoute.getLogin()} />
+        <Route exact path="/" component={PageMain} />
       </Switch>
     </Router>);
 };
 
-App.propTypes = {};
+App.propTypes = {
+  isAuth: PropTypes.bool
+};
 
-export default App;
+const mapStateToProps = (state) => ({
+  isAuth: getAuthorizationStatus(state)
+})
+
+export default connect(mapStateToProps)(App);
---
+++ src/components/private-route/private-route.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route, Redirect } from 'react-router-dom';
+
+const PrivateRoute = ({ render, path, require, to }) => {
+  return <Route path={path} exact render={() => require ? render() : <Redirect to={to} /> } />
+};
+
+PrivateRoute.propTypes = {
+  render: PropTypes.func,
+  path: PropTypes.string,
+  require: PropTypes.bool,
+  to: PropTypes.string
+}
+
+export default PrivateRoute;
