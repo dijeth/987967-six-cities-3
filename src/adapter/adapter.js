e0bffe271aaad8b99bef48a5dc22ed8931bbdb18
Dmitriy <d.orlov777@gmail.com>
2020-03-22 13:53:10 +0300

    changes adapter because eslint

--- src/adapter/adapter.js
+++ src/adapter/adapter.js
@@ -1,81 +1,77 @@
-import { object } from "prop-types";
-
 const FavoriteStatus = {
   ON: `1`,
   OFF: `0`
 };
 
-const get = (object, path) => {
+const get = (object, path, defaultValue) => {
   const paths = path.split(`.`);
-  let result = object;
+  const result = paths.reduce((accu, elem) => {
+    return accu === undefined ? undefined : accu[elem];
+  }, object);
 
-  path.forEach((it) => {
-    result = result[it];
-
-    if (result === undefined) {
-      return undefined
-    };
-  });
-
-  return result;
-}
+  return result === undefined ? defaultValue : result;
+};
 
 const Converter = {
-  getComment: raw => ({
-    id: String(raw.id || ``),
-    userName: raw.user?.name || ``,
-    userPicture: raw.user?.`avatar_url` || ``,
-    userID: raw.user?.id || ``,
-    isSuperUser: raw.user?.[`is_pro`] || false,
-    rating: raw.rating || 0,
-    description: raw.comment || ``,
-    date: raw.date || ``
+  getComment: (raw) => ({
+    id: String(get(raw, `id`, ``)),
+    userName: get(raw, `user.name`, ``),
+    userPicture: get(raw, `user.avatar_url`, ``),
+    userID: get(raw, `user.id`, ``),
+    isSuperUser: get(raw, `user.is_pro`, false),
+    rating: get(raw, `rating`, 0),
+    description: get(raw, `comment`, ``),
+    date: get(raw, `date`, ``),
   }),
 
-  getUser: raw => ({
-    id: String(raw.id || ``),
-    userPic: raw[`avatar_url`] || ``,
-    email: raw.email || ``,
-    isSuperUser: raw[`is_pro`] || false
+  getUser: (raw) => ({
+    id: String(get(raw, `id`, ``)),
+    userPic: get(raw, `avatar_url`, ``),
+    email: get(raw, `email`, ``),
+    isSuperUser: get(raw, `is_pro`, false),
   }),
 
-  getOffer: raw => ({
-    id: String(raw.id || ``),
-    city: raw.city?.name || ``,
-    pictures: (raw[`preview_image`] ? [raw[`preview_image`]] : []).concat(
-      raw.images || []
-    ),
-    title: raw.title || ``,
+  getOffer: (raw) => ({
+    id: String(get(raw, `id`, ``)),
+    city: get(raw, `city.name`, ``),
+
+    pictures: (() => {
+      const firstImage = get(raw, `preview_image`, null);
+      const images = get(raw, `images`, []);
+      return (firstImage === null ? [] : [firstImage]).concat(images);
+    })(),
+
+    title: get(raw, `title`, ``),
     descriptionTitle: `Meet the host`,
-    isFavorite: raw[`is_favorite`] || false,
-    isPremium: raw[`is_premium`] || false,
-    rating: raw.rating || 0,
-    type: raw.type || ``,
-    bedroomCount: raw.bedrooms || 0,
-    adultsCount: raw[`max_adults`] || 0,
-    cost: raw.price || 0,
-    insideFeatures: raw.goods || [],
-    userName: raw.host?.name || ``,
-    userPicture: raw.host?.[`avatar_url`] || ``,
-    isSuperUser: raw.host?.[`is_pro`] || false,
-    userID: String(raw.host?.id || ``),
-    description: raw.description || ``,
-    coord: [raw.location?.latitude || 0, raw.location?.longitude || 0],
-    zoom: raw.location?.zoom || 0
+    isFavorite: get(raw, `is_favorite`, false),
+    isPremium: get(raw, `is_premium`, false),
+    rating: get(raw, `rating`, 0),
+    type: get(raw, `type`, ``),
+    bedroomCount: get(raw, `bedrooms`, 0),
+    adultsCount: get(raw, `max_adults`, 0),
+    cost: get(raw, `price`, 0),
+    insideFeatures: get(raw, `goods`, []),
+    userName: get(raw, `host.name`, ``),
+    userPicture: get(raw, `host.avatar_url`, ``),
+    isSuperUser: get(raw, `host.is_pro`, false),
+    userID: String(get(raw, `host.id`, ``)),
+    description: get(raw, `description`, ``),
+    coord: [get(raw, `location.latitude`, 0), get(raw, `location.longitude`, 0)],
+    zoom: get(raw, `location.zoom`, 0),
   }),
 
-  getCity: raw => ({
-    name: raw.city?.name || ``,
-    zoom: raw.city?.location?.zoom || 0,
+  getCity: (raw) => ({
+    name: get(raw, `city.name`, ``),
+    zoom: get(raw, `city.location.zoom`, 0),
     centerCoord: [
-      raw.city?.location?.latitude || 0,
-      raw.city?.location?.longitude || 0
+      get(raw, `city.location.latitude`, 0),
+      get(raw, `city.location.longitude`, 0),
     ]
   })
 };
 
 const Adapter = {
-  getData: rawData => {
+  getData: (rawData) => {
     if (!Array.isArray(rawData)) {
       rawData = [];
     }
@@ -83,29 +79,29 @@ const Adapter = {
     const offers = [];
     const cities = {};
 
-    rawData.forEach(it => {
+    rawData.forEach((it) => {
       offers.push(Converter.getOffer(it));
 
       const city = Converter.getCity(it);
       cities[city.name] = city;
     });
 
-    return { offers, cities: Array.from(Object.values(cities)) };
+    return {offers, cities: Array.from(Object.values(cities))};
   },
 
-  getOffer: raw => Converter.getOffer(raw),
+  getOffer: (raw) => Converter.getOffer(raw),
 
-  getUser: rawData => Converter.getUser(rawData),
+  getUser: (rawData) => Converter.getUser(rawData),
 
-  getComments: rawData => {
-    if (! Array.isArray(rawData)) {
-      rawData = []
-    };
+  getComments: (rawData) => {
+    if (!Array.isArray(rawData)) {
+      rawData = [];
+    }
 
-    return rawData.map(it => Converter.getComment(it));
+    return rawData.map((it) => Converter.getComment(it));
   },
 
-  postFavorite: isFavorite =>
+  postFavorite: (isFavorite) =>
     isFavorite ? FavoriteStatus.ON : FavoriteStatus.OFF
 };
 
